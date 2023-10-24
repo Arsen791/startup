ca80a3c1b8aa5a0e1d76410bab7b16d9dfdd8ce1
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-10-24 12:08:40 +0600

    edited

--- project/models.py
+++ project/models.py
@@ -8,7 +8,6 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     class Meta:
         verbose_name = 'Blog'
